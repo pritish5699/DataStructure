d85fecfec54357336933641ef53e611a13c0df74
pshah <pritishshah.it@gmail.com>
2014-06-24 16:00:16 -0700

    skeleton of method. Actual implementation pending.

--- Heap/src/main/java/com/pshah/ds/heap/Heap.java
+++ Heap/src/main/java/com/pshah/ds/heap/Heap.java
@@ -1,5 +1,50 @@
 package com.pshah.ds.heap;
 
 public class Heap {
-
+	
+	private Node[] heaparray;
+	private int maxsize;
+	private int currentSize;
+	
+	public Heap(int max)
+	{
+		heaparray = new Node[max];
+		currentSize = 0;
+		maxsize = max;
+	}
+	
+	public boolean isEmpty()
+	{
+		return currentSize==0;
+	}
+	
+	public boolean insert(int key)
+	{
+		return true;
+	}
+	
+	public Node remove()
+	{
+		return null;
+	}
+	
+	public boolean change(int index, int newValue)
+	{
+		return true;
+	}
+	
+	public void trickleUp(int index)
+	{
+		
+	}
+	
+	public void trickleDown(int index)
+	{
+		
+	}
+	
+	public void displayHeap()
+	{
+		
+	}
 }
---
+++ Heap/src/main/java/com/pshah/ds/heap/Node.java
@@ -0,0 +1,11 @@
+package com.pshah.ds.heap;
+
+public class Node {
+	
+	public int idata;
+	
+	public Node(int idata)
+	{
+		this.idata = idata;
+	}
+}
