2c2f3a8da12f11d520b51d0cd4bcac00c89c9668
pshah <pritishshah.it@gmail.com>
2014-06-26 08:38:48 -0700

    implementation of functions.

--- Heap/src/main/java/com/pshah/ds/heap/Heap.java
+++ Heap/src/main/java/com/pshah/ds/heap/Heap.java
@@ -20,27 +20,67 @@ public class Heap {
 	
 	public boolean insert(int key)
 	{
+		if(currentSize >= maxsize)
+			return false;
+		Node node = new Node(key);
+		heaparray[currentSize] = node;
+		trickleUp(currentSize++);
 		return true;
 	}
 	
 	public Node remove()
 	{
-		return null;
+		Node root = heaparray[0];
+		heaparray[0] = heaparray[--currentSize];
+		trickleDown(0);
+		return root;
 	}
 	
 	public boolean change(int index, int newValue)
 	{
+		if(index < 0 || index >= currentSize)
+			return false;
+		int oldValue = heaparray[index].idata;
+		heaparray[index].idata = newValue;
+		if(oldValue < newValue)
+			trickleUp(index);
+		else
+			trickleDown(index);
 		return true;
 	}
 	
 	public void trickleUp(int index)
 	{
-		
+		Node bottom = heaparray[index];
+		int parent = (index - 1) / 2;
+		while(index > 0 && heaparray[parent].idata < bottom.idata)
+		{
+			heaparray[parent] = heaparray[index];
+			index = parent;
+			parent = (parent-1)/2;
+		}
+		heaparray[index] = bottom;
 	}
 	
 	public void trickleDown(int index)
 	{
-		
+		Node top = heaparray[index];
+		int largerchild;
+		while(index < currentSize/ 2)
+		{
+			int leftChild = 2 * index + 1;
+			int rightChild = 2 * index + 2;
+			if(rightChild < currentSize && heaparray[rightChild].idata > heaparray[leftChild].idata)
+				largerchild = rightChild;
+			else
+				largerchild = leftChild;
+			
+			if(top.idata >= heaparray[largerchild].idata)
+				break;
+			heaparray[index] = heaparray[largerchild];
+			index = largerchild;
+		}
+		heaparray[index] = top;
 	}
 	
 	public void displayHeap()
